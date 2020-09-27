5cdb3b15b3b2204c0d45e5a4299434d84098396f
Yang Zhou <zhou.yang.19@ucl.ac.uk>
2020-09-27 15:20:45 +0800

    doc(deepfix): modify comments

--- deepfix-java/init_C_data.sh
+++ deepfix-java/init_C_data.sh
@@ -6,7 +6,7 @@ mkdir data/results
 echo 'make direction finished'
 
 echo 'Make sure you have (prutor-deepfix-09-12-2017.zip) in data/'
-# wget https://www.cse.iitk.ac.in/users/karkare/prutor/prutor-deepfix-09-12-2017.zip -P data/
+wget https://www.cse.iitk.ac.in/users/karkare/prutor/prutor-deepfix-09-12-2017.zip -P data/
 cd data
 unzip prutor-deepfix-09-12-2017.zip
 mv prutor-deepfix-09-12-2017/* iitk-dataset/
@@ -20,8 +20,5 @@ echo 'Preprocessing DeepFix dataset...'
 export PYTHONPATH=.
 python data_processing/preprocess.py
 
-echo "Make sure that your tensorflow is version 1.0.1 before proceeding, checking your tensorflow version now!"
-echo
-python -c 'import tensorflow as tf; print tf.__version__'
 
 echo 'init.sh executed finished'
--- deepfix-java/train_java.sh
+++ deepfix-java/train_java.sh
@@ -1,19 +1,18 @@
-# 需要先开启虚拟环境
+# Activate virtual environment first
 
-# 首先对Java数据进行预处理，将一部分Java数据写入数据库
-# 在这里会创建两张数据表，分别是Code和Benchmark
+# create a direction to store data
+mkdir data/java_data
+
+# First we preporcess Java data, store part of them into a sqlite3 database.
+# In the database, we create two tables, Code and Benchmark
 python data_processing/preprocess_for_java.py 
 
-
-# 在data/checkpoints/Deepfix-Java-seed-1189/路径下生成training data
+# in data/checkpoints/Deepfix-Java-seed-1189/ we generate training data
 python data_processing/java_training_data_generator.py
 
-# 从Benchmark数据表中提取数据，并生成用于最终evaluation的真实数据
-# 要注意一般并不会提取所有的数据，因为有长度范围的限制
 python data_processing/java_test_data_generator.py
 
-# 进行训练
 python -O neural_net/train.py data/network_inputs/Deepfix-Java-seed-1189/bin_1 data/checkpoints/Deepfix-Java-seed-1189/bin_1 -v 0.95
 
-# 进行evaluation 指定checkpoint的路径
 python -O post_processing/generate_java_fixes.py data/checkpoints/Deepfix-Java-seed-1189/bin_1 -v 0.6
+# Do evaluation, speicify path to checkpoint: data/checkpoints/Deepfix-Java-seed-1189/bin_1
